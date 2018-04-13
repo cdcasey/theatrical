d277217d566e80cc3739dfc0a18d17a254eacd75
Chris Casey <cdcasey@gmail.com>
2018-04-13 15:44:15 -0500

    Basic knex config

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+module.exports = {
+
+  test: {
+    client: 'postgres',
+    connection: {
+      filename: 'theatrical-test'
+    }
+  },
+
+  development: {
+    client: 'postgres',
+    connection: {
+      filename: 'theatrical-dev'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
