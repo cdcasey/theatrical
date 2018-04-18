ff63c25f006ae087c92f32d1e29092a6e53a7170
Chris Casey <cdcasey@gmail.com>
2018-04-18 09:39:20 -0500

    Ensuring DB refresh is not run on production

--- db/refresh.sh
+++ db/refresh.sh
@@ -1,8 +1,8 @@
-knex migrate:rollback
+knex migrate:rollback --env development
 knex migrate:rollback --env test
 
-knex migrate:latest
+knex migrate:latest --env development
 knex migrate:latest --env test
 
-knex seed:run
+knex seed:run --env development
 knex seed:run --env test
