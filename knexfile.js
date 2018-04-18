cdfab7be3d8a0b36b4edad55addaf0efa13237d5
Chris Casey <cdcasey@gmail.com>
2018-04-18 09:33:43 -0500

    Yet another db connection try

--- knexfile.js
+++ knexfile.js
@@ -2,50 +2,54 @@
 
 module.exports = {
 
-  test: {
-    client: 'postgresql',
-    connection: {
-      database: 'theatrical-test'
-    }
-  },
+  // test: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'theatrical-test'
+  //   }
+  // },
 
-  development: {
-    client: 'postgresql',
-    connection: {
-      database: 'theatrical-dev'
-    }
-  },
+  // development: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'theatrical-dev'
+  //   }
+  // },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'ec2-54-235-146-184.compute-1.amazonaws.com:5432/d74ijojkjnold',
-      user:     process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'ec2-54-235-146-184.compute-1.amazonaws.com:5432/d74ijojkjnold',
+  //     user:     process.env.DATABASE_USER,
+  //     password: process.env.DATABASE_PASSWORD
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 
 };
